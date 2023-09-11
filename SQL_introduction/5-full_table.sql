e19e6b6114d7d8eea6b39d232aceef1ab35cb510
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-11 23:43:01 +0100

    description of first tsable

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,13 +1,3 @@
--- Create database and table
-
 USE hbtn_0c_0;
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(128),
-    c CHAR(1),
-    created_at DATE,
-    PRIMARY KEY (id)
-);
 
-SELECT * 
-FROM first_table; 
+SHOW TABLES;
