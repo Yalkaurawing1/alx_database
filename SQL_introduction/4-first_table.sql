5c92b824395e95043431718ecb565c32a0a160ce
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-11 22:21:07 +0100

    first table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,9 @@
 --script to creat first table 
 
-
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+DROP DATABASE IF EXISTS hbtn_test_db_4;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_4;
+USE hbtn_test_db_4;
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
