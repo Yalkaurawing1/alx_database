b7d7a228596c0a32c5a69e2a6eb61dc8cea78fa3
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-11 22:44:52 +0100

    first table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -3,6 +3,10 @@
 DROP DATABASE IF EXISTS hbtn_0c_0;
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 USE hbtn_0c_0;
+CREATE TABLE first_table(
+    id INT,
+    name VARCHAR(256)
+);
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
