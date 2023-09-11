48fd554c83518f9836992b9fc93ef4ce7679cc59
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-11 23:28:56 +0100

    description of first table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,4 @@
--- Create database and table
-
-USE hbtn_0c_0;
-
-SELECT * 
-FROM first_table; 
+-- Print the full description of the table first_table
+SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
