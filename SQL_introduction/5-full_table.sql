bef1296d3e73f8f9eb57712f0d5a6820fce61c8e
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-11 23:20:09 +0100

    DESCRIPTION OF FIRST TABLE

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,5 @@
---
+-- Create database and table
 
-SELECT column_name, data_type, character_maximum_length, is_nullable, column_default, extra FROM information_schema.columns WHERE table_name = 'first_table';
+USE hbtn_0c_0;
+
+DESCRIBE first_table; 
