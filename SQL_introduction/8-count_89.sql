0529780edfd338a509a08b85663430c67ba463c0
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-12 00:27:23 +0100

    first_table WITH ID = 89

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,4 +1,5 @@
 --a script that displays the number of records with id = 89 in the table first_table 
 USE hbtn_test_db_8;
-
-SELECT FROM first_table WHERE id = 89;
+SELECT COUNT(id)
+FROM first_table
+WHERE id=89;
