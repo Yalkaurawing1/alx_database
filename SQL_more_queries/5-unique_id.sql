510cd9476248b087e27c60e08ffac552d8c514ff
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-12 23:26:45 +0100

    create a table unique id

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,9 @@
+-- script to create a table unique id 
+
+USE ;
+
+-- Create the table unique_id if it does not exist
+CREATE TABLE IF NOT EXISTS unique_id (
+  id INT DEFAULT 1 UNIQUE,
+  name VARCHAR(256)
+);
