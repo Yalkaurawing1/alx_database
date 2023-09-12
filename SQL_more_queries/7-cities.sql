97b1ebed5cbffa905ab261796cdc4c96919620a5
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-12 23:59:23 +0100

    database hbtn_0d_usa and the table cities

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -6,8 +6,9 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 
 -- Create the table cities if it does not exist
-CREATE TABLE IF NOT EXISTS cities (
+CREATE TABLE IF NOT EXISTS cities(
   id INT AUTO_INCREMENT PRIMARY KEY,
   state_id INT NOT NULL,
   name VARCHAR(256) NOT NULL,
   FOREIGN KEY (state_id) REFERENCES states (id)
+);
