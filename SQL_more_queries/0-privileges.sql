f6174f65358936850f5994214cbdf800994d4d94
Yusuf sani <yalkaurawing@yahoo.com>
2023-09-12 22:28:36 +0100

    show  grant for users

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,11 +1,9 @@
 -- Log in to the MySQL server as root or a user with SELECT permission
 mysql -u root -p
 
--- Delete user user_0d_1 and user_0d_2 if exist
-DROP USER IF EXISTS 'user_0d_1'@'localhost';
-
--- Show the privileges for user_0d_1 
+-- Show the privileges for user_0d_1
 SHOW GRANTS FOR user_0d_1@localhost;
+ 
 
 -- Show the privileges for user_0d_2
 SHOW GRANTS FOR user_0d_2@localhost;
